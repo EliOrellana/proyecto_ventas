4b572b7c834811987a1f27ee0b31d348b2dd7129
EliOrellana <eli.orellana.baez@gmail.com>
2025-02-12 23:47:27 -0300

    Update analisis_ventas.py

--- analisis_ventas.py
+++ analisis_ventas.py
@@ -2,8 +2,7 @@ import pandas as pd
 import numpy as np
 import csv
 
-
-df = pd.read_csv("https://github.com/EliOrellana/proyecto_ventas/blob/ee2f614f418a08338f260a8127c205c8857ac858/ventas_productos.csv")
+df = pd.read_csv("entorno_ventas\ventas_productos.csv")
 print (df)
 
 df ['precio_total'] = df['cantidad'] * df['precio'] 
